d4c17417875a14f0396f00877f80ed8d8d83009d
enomura <enomura@step-japan.co.jp>
2017-01-23 17:55:31 +0900

    damboo

--- public/js/damboo.js
+++ public/js/damboo.js
@@ -7,13 +7,14 @@ var renderer;
 // カメラ
 var camara;
 
-// ライト
-var light;
+// 環境光
+var ambientlight;
 
-/**
- * レンダラ―初期化
- */
-var initRenderer = () => {
+// 平行光
+var directionalLight
+
+/** レンダラ―初期化 */
+var initRenderer = function() {
   var mapCanvas = $('canvas');
   var mapWidth = mapCanvas.width();
   var mapHeight = mapCanvas.height();
@@ -26,41 +27,43 @@ var initRenderer = () => {
     antialias : true,
     canvas : mapCanvas[0]
   });
-  renderer.shadowMap.enabled = true;
+  renderer.shadowMapEnabled  = true;
 }
 
-/**
- * カメラ初期化処理
- */
- var initCamera = () => {
+/** カメラ初期化処理 */
+ var initCamera = function() {
    // 投資投影カメラを作成
-  //  camera = new THREE.PerspectiveCamera( 100, window.innerWidth / window.innerHeight, 1, 1000 );
+   camera = new THREE.PerspectiveCamera( 80, window.innerWidth / window.innerHeight, 1, 1000 );
 
    // 性投影カメラを作成
-   camera = new THREE.OrthographicCamera( -300, 300, 300, -300, 0, 1000 );
+  //  camera = new THREE.OrthographicCamera( -300, 300, 300, -300, 0, 1000 );
 
    // カメラの中心座標(上方向)を設定
    // y方向を上に設定
    camera.up.set(0,1,0);
 
    // カメラ位置を設定
-   camera.position.set(50, 100, 300);
+   camera.position.set(0, 100, 400);
 
    // カメラが移すポイント
-   camera.lookAt({x:0, y:80, z:0 });
+   camera.lookAt({x:0, y:200, z:0 });
  }
 
-/**
- * ライト初期化処理
- */
- var initLight = () => {
+/** ライト初期化処理 */
+ var initLight = function() {
    // 並行光源ライト
-   var directionalLight = new THREE.DirectionalLight('#ffffff', 0.5);
-   directionalLight.position.set(0, 10, 10);
+   directionalLight = new THREE.DirectionalLight('#ffffff', 0.5);
+   directionalLight.position.set(100, 400, 300);
+   scene.add(directionalLight);
 
    // 影有効化
    directionalLight.castShadow = true;
-   scene.add(directionalLight);
+   directionalLight.shadow.camera.left = -(window.innerWidth / 2);
+   directionalLight.shadow.camera.right = window.innerWidth / 2;
+   directionalLight.shadow.camera.top = -(window.innerHeight / 2);
+   directionalLight.shadow.camera.bottom = window.innerHeight / 2;
+   directionalLight.shadow.camera.near = 0;
+   directionalLight.shadow.camera.far = 1100;
 
    // 環境光
    ambientLight = new THREE.AmbientLight(0xffffff,0.7);
@@ -68,93 +71,148 @@ var initRenderer = () => {
 
  }
 
-/**
- * オブジェクト作成処理
- */
- var createObject = () => {
+/** ダンボー作成処理  */
+ var createDamboo = function() {
 
    // 床を作成
-   var ground = createGroundObject();
+   var ground = createGround();
    ground.position.set(0,-40,0);
    scene.add(ground);
 
    // 左足を作成
-   var leftLeg = createLegObject();
+   var leftLeg = createLeg();
    leftLeg.position.set(20,0,0);
    scene.add(leftLeg);
 
-   var rightLeg = createLegObject();
+   // 右足を作成
+   var rightLeg = createLeg();
    rightLeg.position.set(-20,0,0);
    scene.add(rightLeg);
 
-   var body = createBodyObject();
+   // 体を作成
+   var body = createBody();
    body.position.set(0,80,0);
    scene.add(body);
 
+   // 頭を作成
    var head = createHead();
    head.position.set(0,180,0);
    scene.add(head);
+
+   // 右腕を作成
+   var rightArm = createArm();
+   rightArm.position.set(-70,70,0);
+   rightArm.rotation.set(0,0,-Math.PI/16);
+   scene.add(rightArm);
+
+   // 左腕を作成
+   var leftArm = createArm();
+   leftArm.position.set(70,70,0);
+   leftArm.rotation.set(0,0,Math.PI/16);
+   scene.add(leftArm);
+
+   // 口を作成
+   var mouse = createMouse();
+   mouse.position.set(0,150,51);
+   scene.add(mouse);
+
+   // 右目を作成
+   var rightEye = createEye();
+   rightEye.position.set(-30,185,51);
+   scene.add(rightEye);
+
+   // 左目を作成
+   var leftEye = createEye();
+   leftEye.position.set(30,185,51);
+   scene.add(leftEye);
+
  }
 
-var createGroundObject = () => {
-  var geometry = new THREE.BoxGeometry(300, 0, 300);
-  var material = new THREE.MeshPhongMaterial({color: 'rgb(203, 203, 203)'});
-  var ground = new THREE.Mesh( geometry, material );
-  ground.castShadow = true;
-  ground.receiveShadow = true;
-  return ground;
+ /** ボックスメッシュ作成処理 */
+ var createBoxObject = function(x,y,z, color) {
+   var geometry = new THREE.BoxGeometry(x, y, z);
+   var material = new THREE.MeshLambertMaterial({'color': color});
+   var mesh = new THREE.Mesh( geometry, material );
+   mesh.castShadow = true;
+   mesh.receiveShadow = true;
+   return mesh;
+ }
+
+/** 土台作成処理 */
+var createGround = function() {
+  return createBoxObject(300, 0, 300, 'rgb(203, 203, 203)');
 }
 
-var createLegObject = () => {
-  var geometry = new THREE.BoxGeometry(30,80,30);
-  var material = new THREE.MeshPhongMaterial( { color: 'rgb(207, 175, 122)' } );
-  var leg = new THREE.Mesh( geometry, material );
-  leg.castShadow = true;
-  leg.receiveShadow = true;
-  return leg;
+/** 足メッシュ作成処理 */
+var createLeg = function() {
+  return createBoxObject(30,80,30, 'rgb(207, 175, 122)');
 }
 
-var createBodyObject = () => {
-  var geometry = new THREE.BoxGeometry(80,120,50);
-  var material = new THREE.MeshPhongMaterial( { color: 'rgb(207, 175, 122)' } );
-  var body = new THREE.Mesh( geometry, material );
-  body.castShadow = true;
-  body.receiveShadow = true;
-  return body;
+/** 胴体メッシュ作成処理 */
+var createBody = function() {
+  return createBoxObject(80,120,50, 'rgb(207, 175, 122)');
 }
 
-var createHead = ()=> {
-  var geometry = new THREE.BoxGeometry(120,80,80);
-  var material = new THREE.MeshPhongMaterial( { color: 'rgb(207, 175, 122)' } );
-  var head = new THREE.Mesh( geometry, material );
-  head.castShadow = true;
-  head.receiveShadow = true;
-  return head;
+/** 頭メッシュ作成処理 */
+var createHead = function() {
+  return createBoxObject(160,100,100, 'rgb(207, 175, 122)');
 }
 
-/**
- * レンダリング処理
- */
-var render = () => {
+/** 腕メッシュ作成処理 */
+var createArm = function() {
+  return createBoxObject(25,100,25, 'rgb(207, 175, 122)');
+}
+
+/** 口メッシュ作成処理 */
+var createMouse = function() {
+  //ジオメトリ（形状）の宣言と生成
+  var geometry = new THREE.Geometry();
+
+  //頂点座標データを追加
+  geometry.vertices[0] = new THREE.Vector3(0,10,0);
+  geometry.vertices[1] = new THREE.Vector3(-10,0,0);
+  geometry.vertices[2] = new THREE.Vector3(10,0,0);
+
+  //面指定用頂点インデックスを追加
+  geometry.faces[0] = new THREE.Face3(0,1,2);
+
+  //マテリアル（材質）の宣言と生成
+  var material =  new THREE.MeshBasicMaterial({ color: 'rgb(60, 40, 2)' });
+  return  new THREE.Mesh(geometry,material);
+}
+
+/** 目メッシュ作成処理 */
+var createEye = function() {
+  var material = new THREE.MeshBasicMaterial( { color: 'rgb(60, 40, 2)' } );
+  var geometry = new THREE.CircleGeometry( 8, 500 );
+  return new THREE.Mesh( geometry, material );
+}
+
+
+/** レンダリング開始処理 */
+var render = function() {
   if (renderer !== null && renderer !== undefined) {
     requestAnimationFrame(render);
-    //cube.rotation.x += 0.01;
-    //cube.rotation.y += 0.01;
     renderer.render(scene, camera);
   }
 };
 
 
+// ファイルロード完了後実施される処理
+$(function() {
 
-$(() => {
-
+  // レンダラ―初期化
   initRenderer();
 
+  // カメラ初期化
   initCamera();
 
+  // ライト初期化
   initLight();
 
-  createObject();
+  // ダンボー作成
+  createDamboo();
 
+  // レンダリング開始
   render();
 });
